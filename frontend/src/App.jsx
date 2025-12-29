78c1ef679c771d0370ef03c593f0d08c22c7cefe
Nithin <nithin.shsr@gmail.com>
2025-12-29 17:42:52 +0530

    Create a NavBar and a Theme Selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar.jsx"
 import Homepage from "./pages/HomePage.jsx"
 import ProductPages from "./pages/ProductPage.jsx"
+import { useThemeStore } from "./store/useThemeStore.js"
 
 import { Routes, Route } from "react-router-dom"
 function App() {
+    const { theme } = useThemeStore();
     return (
-        <div className="min-h-screen bg-base-200 transition-colors duration-300">
+        <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
             <NavBar/>
 
             <Routes>
